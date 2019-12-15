bfc3d72a6cc4156e87898f1de743727827140241
Rajiv Nair <rajivnairr@gmail.com>
2019-12-15 19:58:59 +1100

    variables chapter included

---
+++ tutorial/1.firststeps/src/variables.java
@@ -0,0 +1,7 @@
+public class variables {
+    public static void main(String[] args){
+        int age = 34;
+        String name = new String("Rajiv Nair");
+        System.out.println("My name is: "+name+" & my age is: "+ age);
+    }
+}
